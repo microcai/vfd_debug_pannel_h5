4bb2ecd09eaa6a863ea36fb3c2129def6a75e204
microcai <microcaicai@gmail.com>
2026-06-14 17:58:38 +0800

    add more baudrates

--- src/app/services/vfd.service.ts
+++ src/app/services/vfd.service.ts
@@ -700,6 +700,6 @@ export class VfdService {
   }
   
   getAvailableBaudRates(): number[] {
-    return [9600, 19200, 38400, 57600, 115200, 230400];
+    return [1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200, 230400];
   }
 }
